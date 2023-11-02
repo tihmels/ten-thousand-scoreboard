6b19ddf3b467ba46b34694de737bfb19eb70b2ac
DasHasan <5646150+DasHasan@users.noreply.github.com>
2023-11-02 21:11:53 +0000

    Add styled typography to GameHeader component

--- src/GameHeader.tsx
+++ src/GameHeader.tsx
@@ -1,6 +1,24 @@
+import { styled } from '@mui/system';
 import Typography from '@mui/material/Typography';
 
+const StyledTypography = styled(Typography)({
+    fontSize: '3em',
+    color: '#ffffff',
+    background: 'linear-gradient(45deg, #FFD700 30%, #FFA500 90%)',
+    boxShadow: '0 3px 5px 2px rgba(255, 215, 0, .3)',
+    padding: '20px 30px',
+    borderRadius: '15px',
+    margin: '20px',
+    textShadow: '2px 2px #000000',
+    fontFamily: 'Comic Sans MS',
+});
+
 const GameHeader = () => {
-    return <Typography align={'center'} variant={'h2'}>10.000</Typography>
+    return (
+        <StyledTypography align={'center'} variant={'h2'}>
+            10.000
+        </StyledTypography>
+    );
 };
+
 export default GameHeader;
