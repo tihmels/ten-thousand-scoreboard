697df5cfd56ba3271f20a093b97a15ab03c032da
Hasan Celik <h.celik@vrame.com>
2023-11-07 18:15:28 +0100

    spin-and-scale-animation Add a spin and scale animation for dice header

--- src/GameHeader.tsx
+++ src/GameHeader.tsx
@@ -1,6 +1,7 @@
 import CasinoIcon from "@mui/icons-material/Casino";
 import { GameState } from "./GameState";
 import { FC } from "react";
+import "./GameHeader.css";
 
 interface IGameHeaderProps {
   gameState: GameState;
@@ -10,12 +11,12 @@ const GameHeader: FC<IGameHeaderProps> = ({ gameState }) => {
   if (gameState === "STARTED") return null;
   return (
     <>
-      <CasinoIcon />
-      <CasinoIcon />
-      <CasinoIcon />
-      <CasinoIcon />
-      <CasinoIcon />
-      <CasinoIcon />
+      <CasinoIcon className="spin-and-scale" />
+      <CasinoIcon className="spin-and-scale" />
+      <CasinoIcon className="spin-and-scale" />
+      <CasinoIcon className="spin-and-scale" />
+      <CasinoIcon className="spin-and-scale" />
+      <CasinoIcon className="spin-and-scale" />
     </>
   );
 };
